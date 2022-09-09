e9baa816603c398d88622d075b22b5ffe1ab1535
Gustavo Gard <gusgard@gmail.com>
2022-09-08 22:02:43 -0300

    Add 2 markers

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,5 @@
-import Image from "next/image";
 import styles from "./Address.module.scss";
 import { Status, Wrapper } from "@googlemaps/react-wrapper";
-import { useRef, useEffect, ReactElement } from "react";
-import { render } from "react-dom";
 import React from "react";
 
 type MyMapComponentProps = {
@@ -39,7 +36,7 @@ const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
 
   React.useEffect(() => {
     if (!marker) {
-      setMarker(new google.maps.Marker());
+      setMarker(new google.maps.Marker({ label: options.label }));
     }
 
     // remove marker from map on unmount
@@ -48,7 +45,7 @@ const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
         marker.setMap(null);
       }
     };
-  }, [marker]);
+  }, [marker, options.label]);
 
   React.useEffect(() => {
     if (marker) {
@@ -60,20 +57,25 @@ const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
 };
 
 // https://stackblitz.com/github/googlemaps/js-samples/tree/sample-react-map?file=index.tsx
-const Render = (status: Status): ReactElement => {
-  if (status === Status.LOADING) return <h3>{status} ..</h3>;
-  if (status === Status.FAILURE) return <h3>{status} ...</h3>;
+const RenderStatus = (status: Status): React.ReactElement => {
+  if (status === Status.LOADING) return <h3>{status}</h3>;
+  if (status === Status.FAILURE) return <h3>{status}</h3>;
   return <></>;
 };
 
 export const Map = () => {
-  const center = { lat: -34.7893193, lng: -56.0817336 };
+  const center = { lat: -34.7893193, lng: -56.0795449 };
+  const church = { lat: -34.7892886, lng: -56.0781287 };
   const zoom = 15;
 
   return (
-    <Wrapper apiKey="AIzaSyBzqr3LF_LGaekgyjh_N-sKxIoplIQMR5E" render={Render}>
+    <Wrapper
+      apiKey="AIzaSyBzqr3LF_LGaekgyjh_N-sKxIoplIQMR5E"
+      render={RenderStatus}
+    >
       <MyMapComponent center={center} zoom={zoom}>
-        <Marker position={center} />
+        <Marker position={center} label="Salon" />
+        <Marker position={church} label="Iglesia" />
       </MyMapComponent>
     </Wrapper>
   );
