6fe433002628b724c728ba004a22fe44de21189d
Gustavo Gard <gusgard@gmail.com>
2022-09-10 23:53:01 -0300

    Use parallax

---
+++ public/main/index.tsx
@@ -0,0 +1,6 @@
+import Names from "./names.svg";
+import CornerTopLeft from "./corner-top-left.svg";
+import CornerBottomRight from "./corner-bottom-right.svg";
+import Leave from "./leaves.svg";
+
+export { Names, CornerTopLeft, Leave, CornerBottomRight };
