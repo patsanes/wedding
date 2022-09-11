2b29859795140bd4a96861866018c82a97492d4d
Patricia Sanes <psanesl93@gmail.com>
2022-09-11 00:40:42 -0300

    Header for desktop

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
 import styles from "./Header.module.scss";
 import { Countdown } from "../Countdown/Countdown";
-import Names from "../../../public/main/names.svg";
+import { isMobile } from "react-device-detect";
+import Names from "../../../public/main/header/names.svg";
+import BranchTopLeft from "../../../public/main/header/branch-top-left.svg";
+import BranchTopRight from "../../../public/main/header/branch-top-right.svg";
+import BranchBottomLeft from "../../../public/main/header/branch-bottom-left.svg";
+import BranchBottomRight from "../../../public/main/header/branch-bottom-right.svg";
 
 export const Header = () => {
+  if (isMobile) {
+    return null;
+  }
+
   return (
-    <>
+    <div className={styles.container}>
+      <BranchTopLeft className={styles.itemTopLeft} />
+      <BranchTopRight className={styles.itemTopRight} />
+      <div className={styles.space}/>
       <Names className={styles.names} />
-      <span className={styles.header}>Hola mundo</span>
-      <Countdown />
-    </>
+      <BranchBottomLeft className={styles.itemBottomLeft} />
+      <BranchBottomRight className={styles.itemBottomRight} />
+    </div>
   );
 };
