ab4a38c62fafdfca39ebb7e67cf0dadce25c5f81
Gustavo Gard <gusgard@gmail.com>
2022-09-13 00:28:03 -0300

    Add mobile

--- src/components/Rsvp/Rsvp.tsx
+++ src/components/Rsvp/Rsvp.tsx
@@ -2,13 +2,13 @@ import { usePlatform } from "../../hooks/usePlatform";
 // import styles from "./Rsvp.module.scss";
 
 export const Rsvp = () => {
-  const { size } = usePlatform();
+  const { size, isMobile } = usePlatform();
   return (
     <>
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSc9jqGhoK4umgUtoKoVdYEBLqof6cqUy0yWJQnMSlGcI--c5Q/viewform?embedded=true"
         width={size.width}
-        height="1350"
+        height={isMobile ? "1550" : "1350"}
         frameBorder="0"
         marginHeight={0}
         marginWidth={0}
