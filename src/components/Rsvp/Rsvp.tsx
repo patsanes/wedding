902c6228fc806d3368e2921aed71cf8e68896051
Gustavo Gard <gusgard@gmail.com>
2022-09-12 23:20:48 -0300

    Add form

--- src/components/Rsvp/Rsvp.tsx
+++ src/components/Rsvp/Rsvp.tsx
@@ -2,5 +2,18 @@ import Image from "next/image";
 import styles from "./Rsvp.module.scss";
 
 export const Rsvp = () => {
-  return <></>;
+  return (
+    <>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSc9jqGhoK4umgUtoKoVdYEBLqof6cqUy0yWJQnMSlGcI--c5Q/viewform?embedded=true"
+        width="640"
+        height="1350"
+        frameBorder="0"
+        marginHeight={0}
+        marginWidth={0}
+      >
+        Cargando formulario https://forms.gle/uKtwyJjqBaAFPRWZ9
+      </iframe>
+    </>
+  );
 };
