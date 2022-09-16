04921fbf4392773f8c2858577260371cb529336e
Patricia Sanes <psanesl93@gmail.com>
2022-09-16 10:19:56 -0300

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,20 +1,53 @@
 import Image from "next/image";
+import WhatsappIcon from "../../../public/main/icons/wp.svg";
+
 import styles from "./Footer.module.scss";
 
+const wpGusNumber = "+598 94 040 117";
+const wpPatNumber = "+598 98 557 755";
+const wpGus = "https://wa.me/59894040117";
+const wpPat = "https://wa.me/59898557755";
+
 export const Footer = () => {
-  return null;
-  // return (
-  //   <footer className={styles.footer}>
-  //     <a
-  //       href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-  //       target="_blank"
-  //       rel="noopener noreferrer"
-  //     >
-  //       Powered by{" "}
-  //       <span className={styles.logo}>
-  //         <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-  //       </span>
-  //     </a>
-  //   </footer>
-  // );
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        {/* Nav */}
+        <div className={styles.navBar}>
+          <h2>Secciones</h2>
+          <a href="#date">Fecha</a>
+          <a href="#address">Lugar</a>
+          <a href="#rsvp">Confirmación asistencia</a>
+          <a href="#gifts">Regalos</a>
+        </div>
+        {/* Contact */}
+        <div className={styles.contactContainer}>
+          <h2>Contactanos</h2>
+          <span className={styles.subtitle}>Si tienes alguna duda no dudes en escribirnos!</span>
+          <div className={styles.numbers}>
+            <a
+              className={styles.whatsapp}
+              onClick={() => window.open(wpGus, "_blank")}
+            >
+              <WhatsappIcon className={styles.logo} />
+              <span> Gus - {wpGusNumber}</span>
+            </a>
+            <a
+              className={styles.whatsapp}
+              onClick={() => window.open(wpPat, "_blank")}
+            >
+              <WhatsappIcon className={styles.logo} />
+              <span>Pat - {wpPatNumber}</span>
+            </a>
+            <a href="mailto:casamientogusypat@gmail.com?subject=Casamiento Gus y Pat&body=Tengo una duda">
+              casamientogusypat@gmail.com
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footerContainer}>
+        <a>Powered by Zuri</a>
+      </div>
+    </footer>
+  );
 };
